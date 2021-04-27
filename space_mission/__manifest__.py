4eff989a6b7a77584060a09939998a03f47029d5
Martin Petrov <mpv@odoo.com>
2021-04-27 12:15:13 -0700

    readded skeleton code for space mission

---
+++ space_mission/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Space Mission',
+    'summary': """Academy app to manage training""",
+    'description': """
+        Academy Module to manage Training:
+        - Courses
+        - Sessions
+        - Attendees
+        """,
+    'author': 'Odoo',
+    'website': 'https://www.odoo.com',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        
+    ],
+    'demo': [
+        'demo/academy_demo.xml'
+    ],
+}
---
+++ space_mission/models/ship.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Ship(models.Model):
+   _name='space.ship'
+   _description='Space ship for space missions'
+
+   
