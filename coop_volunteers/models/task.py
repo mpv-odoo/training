f57ab1a551102b7ace033068008090d16ad26eb9
Martin Petrov <mpv@odoo.com>
2021-04-27 12:58:32 -0700

    messing around with date time object

--- coop_volunteers/models/task.py
+++ coop_volunteers/models/task.py
@@ -6,16 +6,8 @@ class Book(models.Model):
    _name='coop.task'
    _description='Task Model'
 
-   author = fields.Char(string='Author', required=True)
-   editor = fields.Char(string='Editor', required=True)
-   isbn = fields.Char(string='ISBN', required=False)
-   year = fields.integer()
-   genre = fields.Selection(string='Genre',
-                              selection=[
-                                 ('fantasy', 'Fantasy'),
-                                 ('adventure', 'Adventure'),
-                                 ('sci-fi', 'Science Fiction')
-                              ],
-                              copy=False
-                           )
+   name = fields.Char(string='Task Name', required=True)
+   description = fields.Text(string='Editor', required=False)
+   start_time = fields.DateTime()
+   
 
