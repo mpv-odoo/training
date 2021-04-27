0c47d90f210a0c05aef6f8f7e7dc71bc0b033fcf
Martin Petrov <mpv@odoo.com>
2021-04-27 15:33:56 -0700

    added module security

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Odoo Academy',
-    'summary': """Academy app to manage training""",
-    'description': """
-        Academy Module to manage Training:
-        - Courses
-        - Sessions
-        - Attendees
-        """,
-    'author': 'Odoo',
-    'website': 'https://www.odoo.com',
-    'category': 'Training',
-    'version': '0.1',
-    'depends': ['base'],
-    'data': [
-        'views/course_views.xml'
-    ],
-    'demo': [
-        'demo/academy_demo.xml'
-    ],
+   'name': 'Odoo Academy',
+   'summary': """Academy app to manage training""",
+   'description': """
+      Academy Module to manage Training:
+      - Courses
+      - Sessions
+      - Attendees
+      """,
+   'author': 'Odoo',
+   'website': 'https://www.odoo.com',
+   'category': 'Training',
+   'version': '0.1',
+   'depends': ['base'],
+   'data': [
+      'security/academy_security.xml',
+      'security/ir.model.access.csv'
+      'views/course_views.xml'
+   ],
+   'demo': [
+      'demo/academy_demo.xml'
+   ],
 }
