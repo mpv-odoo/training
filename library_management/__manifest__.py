d3616ce81911a0706ddf861fed5c82e532d6fa30
Martin Petrov <mpv@odoo.com>
2021-04-27 12:41:51 -0700

    redid library management module

---
+++ library_management/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Library Management',
+    'summary': """A module to keep track of all books in the library""",
+    'description': """
+         The library module keeps track of:
+         - Authors
+         - Editors
+         - Publisher
+         - year of edition
+         - ISBN
+         - Genre
+      """,
+    'author': 'Odoo',
+    'website': 'https://www.odoo.com',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        
+    ],
+    'demo': [
+    ],
+}
---
+++ library_management/models/book.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Book(models.Model):
+   _name='library.book'
+   _description='Book Model'
+
+   author = fields.Char(string='Author', required=True)
+   editor = fields.Char(string='Editor', required=True)
+   isbn = fields.char(string='ISBN', required=False)
+   year = fields.integer()
+   genre = fields.Selection(string='Genre',
+                              selection=[
+                                 ('fantasy', 'Fantasy'),
+                                 ('adventure', 'Adventure'),
+                                 ('sci-fi', 'Science Fiction')
+                              ],
+                              copy=False
+                           )
+
