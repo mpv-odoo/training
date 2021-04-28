eb9ccb987a250cfddbc2ddd82f893573bc5af4b9
Martin Petrov <mpv@odoo.com>
2021-04-28 10:49:36 -0700

    added security for library

--- library_management/__manifest__.py
+++ library_management/__manifest__.py
@@ -1,24 +1,25 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Library Management',
-    'summary': """A module to keep track of all books in the library""",
-    'description': """
-         The library module keeps track of:
-         - Authors
-         - Editors
-         - Publisher
-         - year of edition
-         - ISBN
-         - Genre
-      """,
-    'author': 'Odoo',
-    'website': 'https://www.odoo.com',
-    'category': 'Training',
-    'version': '0.1',
-    'depends': ['base'],
-    'data': [
-        
-    ],
-    'demo': [
-    ],
+   'name': 'Library Management',
+   'summary': """A module to keep track of all books in the library""",
+   'description': """
+      The library module keeps track of:
+      - Authors
+      - Editors
+      - Publisher
+      - year of edition
+      - ISBN
+      - Genre
+   """,
+   'author': 'Odoo',
+   'website': 'https://www.odoo.com',
+   'category': 'Training',
+   'version': '0.1',
+   'depends': ['base'],
+   'data': [
+      'security/library_security.xml',
+      'security/ir.model.access.csv'
+   ],
+   'demo': [
+   ],
 }
