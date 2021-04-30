f1b9bd37ef8f688d883448ed0af2e60ca7bee716
Martin Petrov <mpv@odoo.com>
2021-04-30 14:58:44 -0700

    added reports

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -21,7 +21,8 @@
       'views/session_views.xml',
       'views/sale_views_inherit.xml',
       'views/product_views_inherit.xml',
-      'wizard/sale_wizard_view.xml'
+      'wizard/sale_wizard_view.xml',
+      'reports/session_report_templates.xml'
    ],
    'demo': [
       'demo/academy_demo.xml'
