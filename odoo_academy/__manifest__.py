235b0642af74667a5fa6d586d9865e77c8e7f73c
Martin Petrov <mpv@odoo.com>
2021-04-29 18:09:01 -0700

    following wizards tutorial

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -19,7 +19,8 @@
       'views/academy_menuitems.xml',
       'views/course_views.xml',
       'views/session_views.xml',
-      'views/sale_views_inherit.xml'
+      'views/sale_views_inherit.xml',
+      'views/product_views_inherit.xml'
    ],
    'demo': [
       'demo/academy_demo.xml'
